444b1aa73ac4cd6a5ccaf3f0a608be5730e3af65
Nenad Jovanovic <n.jovanovic@integra-eng.com>
2021-01-17 02:17:29 +0100

    first commit

---
+++ app/controllers/Pages.php
@@ -0,0 +1,20 @@
+
+<?php
+class Pages extends Controller {
+
+    public function __construct() {
+        //$this->userModel = $this->model('User');    --> call model method fron config.php file and User class from model
+    }
+
+    public function index() {
+        // a method from User model
+        // $users = $this->userModel->getUsers();  
+
+
+        // $data = [
+        //     'users' => $users 
+        // ];
+
+        // $this->view('pages/index', $data);
+    }
+}
---
+++ app/views/pages/index.php
@@ -0,0 +1,11 @@
+<?php
+    // echo $data['title'];
+    // echo APPROOT;
+    //echo URLROOT;
+
+
+    // foreach ($data['users'] as $user) {
+    //     echo $user->firstName . " " . $user->lastName;
+    //     echo "<br>";
+    // }
+?>
